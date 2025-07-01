8a01777c1ef4b3146c322f94a987c5961d1534f4
Bogdan1Gavrilov <gavrilov2006b@yandex.ru>
2025-07-01 14:31:54 +0300

    Написал базовую модель на CIFAR10, accuracy 62%

---
+++ evaluate.py
@@ -0,0 +1,49 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+from models.model import CIFAR10CNN 
+from src.prepare_data import get_data_loaders
+
+
+def evaluate(model, device, test_loader, classes):
+    model.eval()
+    all_preds = []
+    all_labels = []
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, preds = torch.max(outputs, 1)
+
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+    acc = accuracy_score(all_labels, all_preds)
+    print(f"Test accuracy: {acc*100:.2f}%")
+
+    cm = confusion_matrix(all_labels, all_preds)
+    plt.figure(figsize=(10,8))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=classes, yticklabels=classes)
+    plt.xlabel("Предсказание")
+    plt.ylabel("Реальность")
+    plt.title("Матрица ошибок модель1")
+    plt.tight_layout()
+    plt.savefig('runs/confusion_matrixV1.png')
+    plt.show()
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_loader, test_loader = get_data_loaders()
+    classes = train_loader.dataset.classes
+    model = CIFAR10CNN()
+    model.load_state_dict(torch.load('weights/cifar10_silu.pth', map_location=device))
+    model.to(device)
+
+    evaluate(model, device, test_loader, classes)
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,66 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+
+from src.prepare_data import get_data_loaders
+from models.model import CIFAR10CNN
+
+#1. Настраиваем устройство и гиперпараметры
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+epochs = 10
+lr = 0.001
+batch_size = 64
+
+#2.Даталоадер
+train_loader, test_loader = get_data_loaders(batch_size=batch_size)
+
+#3.Функция потерь, модель, оптимизатор
+
+model = CIFAR10CNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+#4. Логгер
+writer = SummaryWriter(log_dir="runs/cifar10_exp")
+
+#5. Цикл обучения
+for epoch in range(epochs):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        #Обнуляем градиенты
+        optimizer.zero_grad()
+
+        #Прямой проход
+        outputs = model(images)
+
+        #Потери и градиенты
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        #Статистика
+        running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+    epoch_loss = running_loss / len(train_loader)
+    epoch_acc = 100. * correct/total
+
+    print(f"Epoch [{epoch+1}/{epochs}] Loss: {epoch_loss:.4f} | Accuracy: {epoch_acc:.2f}%")
+    writer.add_scalar("Loss/train", epoch_loss, epoch)
+    writer.add_scalar("Accuracy/train", epoch_acc, epoch)
+
+#6.Сохранение весов модели
+os.makedirs("weights", exist_ok=True)
+torch.save(model.state_dict(), "weights/cifar10_silu.pth")
+
+writer.close()
