0e444688fbef78ebd8dcda15125c83aa8045a4e2
Bogdan1Gavrilov <gavrilov2006b@yandex.ru>
2025-07-02 09:31:31 +0300

    Добавил аугментацию тренировочных данных, подняв Acc на тестовой выборке до 72% без изменения модели

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -6,9 +6,14 @@ import matplotlib.pyplot as plt
 def get_data_loaders(batch_size=64):
     # Трансформации для датасета
     transform = transforms.ToTensor()
-
+    transformAugmentation = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2),
+        transforms.ToTensor(),
+    ])
     # Загрузка CIFAR10
-    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transformAugmentation)
     test_dataset  = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
     # Создание DataLoader-ов
